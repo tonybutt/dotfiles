24cb070bfcfac3493b42dbf781c04615c764565b
Anthony Butt <anthony@abutt.io>
2024-06-10 00:53:08 -0400

    Add git config for commit signing

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -5,6 +5,9 @@
     userEmail = "anthony@abutt.io";
     extraConfig = {
       init.defaultBranch = "main";
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+      user.signingkey = "4C66EAE99B0950AAEED2133CF56C1FE0C44B03BE";
     };
   };
 }
