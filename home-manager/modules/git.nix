bcb03c15bd7262bec498e6c33d0a084f59a4ba73
Anthony Butt <anthony@abutt.io>
2024-06-10 01:35:06 -0400

    Move signing keys to proper struct

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -3,11 +3,13 @@
     enable = true;
     userName = "Anthony Butt";
     userEmail = "anthony@abutt.io";
+    signing = { 
+      key = "4C66EAE99B0950AAEED2133CF56C1FE0C44B03BE";
+      signByDefault = true;
+      gpgPath = "gpg";
+    };
     extraConfig = {
       init.defaultBranch = "main";
-      commit.gpgsign = true;
-      gpg.program = "gpg";
-      user.signingkey = "4C66EAE99B0950AAEED2133CF56C1FE0C44B03BE";
     };
   };
 }
