197ddbdeaa1d4546d2034e124b0ade14ccc35030
Anthony Butt <anthony@abutt.io>
2024-06-09 23:24:16 -0400

    Turn off ssh agent and enable gpg agent replacement

--- nixos/modules/yubikey.nix
+++ nixos/modules/yubikey.nix
@@ -1,10 +1,16 @@
 {pkgs, ...}: {
-  
+
+programs.ssh.startAgent = false;  
 services.pcscd.enable = true;
 
 environment.systemPackages = with pkgs; [
   gnupg
   yubikey-manager
 ];
+
+environment.shellInit = ''
+  gpg-connect-agent /bye
+  export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+'';
 
 }
